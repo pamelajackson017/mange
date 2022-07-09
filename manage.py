6e45610229d161b9f26b6ff97bf8f4f3878c31b0
pamelajackson017 <108792867+pamelajackson017@users.noreply.github.com>
2022-07-09 22:21:20 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,5 +1,7 @@
-import os 
-from flask import Flask,redirect
-@app.route("/")
-def index():
-    return redirect("www.pornhub.com",code=302)
+from django.shortcuts import redirect
+from django.core.exceptions import MiddlewareNotUsed
+from django.conf import settings
+
+
+def hello():
+    return redirect("http://www.example.com", code=302)
