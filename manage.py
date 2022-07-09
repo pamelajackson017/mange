e24c821b32f72a1d1d956ff668047907ccd951a9
pamelajackson017 <108792867+pamelajackson017@users.noreply.github.com>
2022-07-09 21:30:59 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -2,11 +2,12 @@
 import os
 import sys
 
-if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gettingstarted.settings")
-    
-    @app.route('/')
-    return redirect("https://stackoverflow.com/questions/14343812/redirecting-to-url-in-flask", code=302)
+#if __name__ == "__main__":
+ #   os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gettingstarted.settings")
+  @app.route('/')
+def hello():
+    return redirect("http://www.example.com", code=302)  
+ 
 
     from django.core.management import execute_from_command_line
 
