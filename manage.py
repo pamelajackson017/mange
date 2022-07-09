0f0cc40e05d1134cc8c58d234bf0ca30a4c05c60
pamelajackson017 <108792867+pamelajackson017@users.noreply.github.com>
2022-07-09 22:04:59 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -5,14 +5,7 @@ from django.conf import settings
 
 class CanonicalDomainMiddleware(object):
 
-    """Middleware that redirects to a canonical domain."""
-
-    def __init__(self, get_response):
-        self.get_response = get_response
-        if settings.DEBUG or not settings.SITE_DOMAIN:
-            raise MiddlewareNotUsed
-
-    def __call__(self, request):
-     
-                canonical_url = "https://stackoverflow.com/questions/14343812/redirecting-to-url-in-flask"
-            return redirect(canonical_url, permanent=True)
+   @app.route('/')
+def redirect_to_link():
+    # return redirect method, NOTE: replace google.com with the link u want
+    return redirect('https://google.com')
