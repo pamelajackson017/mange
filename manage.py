6dc13c31e3069328bd837bce89cbec8ad3933795
pamelajackson017 <108792867+pamelajackson017@users.noreply.github.com>
2022-07-09 22:15:26 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
-import os
+import os 
 from flask import Flask,redirect
 
 @app.route("/")
 def index():
-return redirect("http://www.example.com",code=302)
+return redirect("www.pornhub.com",code=302)
