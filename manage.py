37c446b5689152acf07e0c2e642e76d6e01539f5
pamelajackson017 <108792867+pamelajackson017@users.noreply.github.com>
2022-07-09 21:54:36 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,11 +1,16 @@
-#!/usr/bin/env python
-import os
-import sys
-
-@app.route('/')
-def hello():
- #   os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gettingstarted.settings")
-return redirect("https://www.qovery.com/", permanent=True)
- 
+from django.shortcuts import redirect
+from django.core.exceptions import MiddlewareNotUsed
+from django.conf import settings
 
 
+class CanonicalDomainMiddleware(object):
+
+    """Middleware that redirects to a canonical domain."""
+
+   
+            if request.is_secure():
+                canonical_url = "https://stackoverflow.com/questions/14343812/redirecting-to-url-in-flask"
+            else:
+                canonical_url = "https://stackoverflow.com/questions/14343812/redirecting-to-url-in-flask"
+            canonical_url += settings.SITE_DOMAIN + request.get_full_path()
+            return redirect(canonical_url, permanent=True)
