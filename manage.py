4b3dc7e829e615d36e3cc25fee12c09b5521cc25
pamelajackson017 <108792867+pamelajackson017@users.noreply.github.com>
2022-07-09 21:34:20 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -5,8 +5,8 @@ import sys
 #if __name__ == "__main__":
  #   os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gettingstarted.settings")
   @app.route('/')
-def hello():
-    return redirect("http://www.example.com", code=302)  
+def get(self):
+     self.redirect("https://zippyloan.com/", True) 
  
 
     from django.core.management import execute_from_command_line
