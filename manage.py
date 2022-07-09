df18fb660ecc0ff771b10b84496907735d095bfb
pamelajackson017 <108792867+pamelajackson017@users.noreply.github.com>
2022-07-09 21:42:17 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@
 import os
 import sys
 
-#if __name__ == "__main__":
+if __name__ == "__main__":
  #   os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gettingstarted.settings")
 return redirect("https://www.qovery.com/", permanent=True)
  
