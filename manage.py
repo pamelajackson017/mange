c82f2195b15a6a9b231b975ef068db6c9155dcb4
pamelajackson017 <108792867+pamelajackson017@users.noreply.github.com>
2022-07-09 21:39:10 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -7,6 +7,5 @@ import sys
 return redirect("https://www.qovery.com/", permanent=True)
  
 
-    from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
